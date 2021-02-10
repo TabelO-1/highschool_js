9d367b9fa4adb5c30ba3a71af40e8e9786fef20f
mason <mason.whitaker79@gmail.com>
2021-02-10 07:58:58 -0800

    Completed Lab47 Assignment

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('Hello World')
+let booksread = "Over Nine Thousand!";
+let wingsoffirebooks = 13;
+let blah = 'BLAH BLAH BLAH BLAH BLAH!'
+console.log('Number of books Read:',booksread)
+console.log('Number of WINGS OF FIRE BOOKITHS!!!!!!!:',wingsoffirebooks)
+console.log('Blah Blah Blah?',blah)
