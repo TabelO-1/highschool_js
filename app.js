07add22be5d502e23ef67718d4ef43b8214c1b4d
mason <mason.whitaker79@gmail.com>
2021-02-10 08:07:01 -0800

    Finished Lab48

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ console.log('Hello World')
 let booksread = "Over Nine Thousand!";
 let wingsoffirebooks = 13;
 let blah = 'BLAH BLAH BLAH BLAH BLAH!'
-console.log('Number of books Read:',booksread)
-console.log('Number of WINGS OF FIRE BOOKITHS!!!!!!!:',wingsoffirebooks)
-console.log('Blah Blah Blah?',blah)
+function firststatement() {
+    console.log('Number of books Read:',booksread)
+}
+function secondstatement() {
+    console.log('Number of WINGS OF FIRE BOOKITHS!!!!!!!:',wingsoffirebooks)
+}
+function thirdstatement() {
+    console.log('Blah Blah Blah?',blah)
+}
+firststatement()
+secondstatement()
+thirdstatement()
