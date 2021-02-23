4b27bfa2124843168f12a7edcc098d4a33f5e4d0
mason <mason.whitaker79@gmail.com>
2021-02-23 08:13:32 -0800

    Finsihed Lab51

--- loop.js
+++
@@ -1,3 +0,0 @@
-// for (var readinghours = 1; readinghours <= 10; readinghours++) {
-//    console.log("I Have spent",readinghours, "Hours Reading");
-//  } 
---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('Hello World')
+let favbooks = ['Series: Wings Of Fire','Favorite Book: Book Thirteen, The Poison Jungle','Series: The Unwanteds','Favorite Book: Book Six, The Island Of Graves','Series: The Unwanteds Quest','Favorite Book: Book Six, Dragon Slayers','Series: The Faithful and The Fallen','Favorite book: Book Four, Wrath','Series: The Six Of Crows Duology','Favorite Book: The Crooked Kingdom']
+console.log(favbooks[0])
+console.log(favbooks[9])
+console.log(favbooks[0], favbooks[9])
+let randomNumber = [69, 21, 36, 7, 111]
+console.log(randomNumber[1] - randomNumber[2])
+console.log(randomNumber[0] * randomNumber[1] * randomNumber[4])
