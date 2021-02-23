1240185b3f61da8cf3b6f4b4289e231c19f93b3c
mason <mason.whitaker79@gmail.com>
2021-02-23 08:29:51 -0800

    Finished Lab52

---
+++ working-with-arrays.js
@@ -0,0 +1,21 @@
+console.log('Hello_World')
+//Navigate back to the repository you created yesterday. 
+//Create a new branch and add a working-with-arrays.js 
+//file for todays work.
+//Create an array of 10 random numbers
+//Using a for loop, loop through your numbers array 
+//and console.log each number squared 
+//(multiply that number by itself)
+let numbers = [69,21,36,64,96,72,7,42,49,12]
+for (let i=0; i < numbers.length; i++) {
+    console.log(numbers[0]*numbers[0])
+    console.log(numbers[1]*numbers[1])
+    console.log(numbers[2]*numbers[2])
+    console.log(numbers[3]*numbers[3])
+    console.log(numbers[4]*numbers[4])
+    console.log(numbers[5]*numbers[5])
+    console.log(numbers[6]*numbers[6])
+    console.log(numbers[7]*numbers[7])
+    console.log(numbers[8]*numbers[8])
+    console.log(numbers[9]*numbers[9])
+}
